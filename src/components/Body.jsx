8ab67b176bd8b238d465775a3f3297c9e7b824be
dennoh-mzoefu <denisgichana162@gmail.com>
2023-09-08 18:00:00 +0200

    body structure

---
+++ src/components/Body.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div>
+      <div className="p-4 sm:ml-64">
+        <div className="p-4  mt-14">
+          <div className="grid grid-cols-3 gap-4 mb-4">
+            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-stone-200"></div>
+          </div>
+          <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-stone-200"></div>
+          <div className="grid grid-cols-2 gap-4 mb-4">
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+          </div>
+          <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-stone-200"></div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-stone-200"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
